10683394780b48fd964ee756031e251d179c080f
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-04-18 23:17:36 +0200

    feat: Add POST endpoint for creating new courses - Implements a POST endpoint to allow users to add new courses.

--- src/main/java/com/courses/crudspring/controller/CourseController.java
+++ src/main/java/com/courses/crudspring/controller/CourseController.java
@@ -1,10 +1,13 @@
 package com.courses.crudspring.controller;
 
-
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.courses.crudspring.model.Course;
@@ -23,4 +26,10 @@ public class CourseController {
   public List<Course> list(){
     return courseRepository.findAll();
   }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Course create(@RequestBody Course course){
+    return courseRepository.save(course);
+  } 
 }
